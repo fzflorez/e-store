db81c1d5dd11a79daf3a0655bb0eb2dc891fb848
Mauricio Florez <106109649+fzflorez@users.noreply.github.com>
2025-09-22 13:12:34 -0500

    add the hero section

--- app/page.tsx
+++ app/page.tsx
@@ -5,19 +5,20 @@ import Link from "next/link";
 
 export default async function Home() {
   const user = await getUser();
-
+  const fullName = user?.user_metadata.fullName;
   return (
-    <section className="bg-gradient-to-br from-slate-50 to-slate-100 py-20 dark:from-slate-900 dark:to-slate-800">
-      <div className="container mx-auto px-4 text-center">
-        <h1 className="mb-6 text-4xl font-bold text-balance md:text-6xl">
+    <section className="bg-secondary py-20">
+      <div className="container mx-auto flex flex-col items-center justify-center gap-8 px-5 text-center">
+        <h1 className="text-4xl font-bold md:text-6xl">
           Bienvenido a nuestra tienda
         </h1>
-        <p className="max-w-3xl text-xl">
+        <p className="text-accent-foreground max-w-2xl text-lg md:text-xl">
           Descubre productos increíbles con los mejores precios y calidad.
-          ¡Únete hoy y aprovecha ofertas exclusivas!
-          {user ? `Bienvenido, ${user.email}` : "Desconocido"}
+          {user
+            ? ` Bienvenido, ${fullName}!`
+            : "¡Únete hoy y aprovecha ofertas exclusivas!"}
         </p>
-        <Button asChild className="text-lg">
+        <Button asChild className="text-base md:text-lg">
           <Link href="/products">
             <ShoppingBag />
             Comprar ahora
